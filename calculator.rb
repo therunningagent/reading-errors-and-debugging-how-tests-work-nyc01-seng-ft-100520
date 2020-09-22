65a61e99abc8c8e98cdb047908630e77c06d08ea
Luis Torres <luistjr@gmail.com>
2020-09-22 16:52:37 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -8,4 +8,6 @@ sum = first_number + second_number
 
 difference = first_number - second_number
 
-multiply = first_number * second_number
+product = first_number * second_number
+
+quotient = first_number / second_number
